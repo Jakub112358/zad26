f5043914084871ee8e9a386d8b92c942aae07ceb
Jakub112358 <jakubszymanski@hotmail.com>
2022-12-08 20:42:50 +0100

    added methods 1-6

--- src/CarMethods.java
+++ src/CarMethods.java
@@ -8,12 +8,12 @@ import java.util.stream.Collectors;
 //
 //Wykorzystując mechanizmy programowania funkcyjnego na podstawie zadanej struktury wyświetl:
 //
-//    listę wszystkich Modelów,
-//    listę wszystkich aut,
-//    listę wszystkich nazw producentów,
-//    listę wszystkich lat założenia producentów,
-//    listę wszystkich nazw modeli,
-//    listę wszystkich lat startu produkcji modeli,
+
+
+
+
+
+
 //    listę wszystkich nazw aut,
 //    listę wszystkich opisów aut,
 //    tylko modele z parzystym rokiem startu produkcji,
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
 //    tylko auta typu SEDAN z modelu nowszego niż 2019 oraz rokiem założenia producenta mniejszym niż 1919.
 public class CarMethods {
 
-    //1
+    //1     listę wszystkich Modelów,
     public List<Model> getAllModels(Set<Manufacturer> manufacturers){
         //najpierw tworzymy strumien, teraz w strumieniu mamy poszczegolnych producentow
         return manufacturers.stream()
@@ -34,12 +34,37 @@ public class CarMethods {
                 .collect(Collectors.toList()); //składamy modele do listy, ktora jest zwracana z metody
     }
 
-    //2
+    //2     listę wszystkich aut,
     public List<Car> getAllCars(Set<Manufacturer> manufacturers){
         return manufacturers.stream()
                 .flatMap(m->m.models.stream()) //mamy teraz w strumieniu modele
                 .flatMap(m->m.cars.stream()) //teraz dostalismy az do samochodow
                 .collect(Collectors.toList());
     }
-
+//   3 listę wszystkich nazw producentów,
+    public List<String> getAllProducerNames(Set<Manufacturer> manufacturers){
+        return manufacturers.stream()
+                .map(m -> m.name)
+                .collect(Collectors.toList());
+    }
+    //   4 listę wszystkich lat założenia producentów,
+    public List<Integer> getAllYearsOfCreation(Set<Manufacturer> manufacturers){
+        return manufacturers.stream()
+                .map(m -> m.yearOfCreation)
+                .collect(Collectors.toList());
+    }
+    //  5  listę wszystkich nazw modeli,
+    public List<String> getAllNamesOfModels(Set<Manufacturer> manufacturers){
+        return manufacturers.stream()
+                .flatMap(m -> m.models.stream())
+                .map( m -> m.name)
+                .collect(Collectors.toList());
+    }
+    //  6  listę wszystkich lat startu produkcji modeli,
+    public List<Integer> getAllProductionStartYears (Set<Manufacturer> manufacturers){
+        return manufacturers.stream()
+                .flatMap(m->m.models.stream())
+                .map(m -> m.productionStartYear)
+                .collect(Collectors.toList());
+    }
 }
